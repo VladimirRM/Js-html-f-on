8adaa84a2e1f151ddda8873b599ba243b737102d
Vladimir RM <shvdrummer@gmail.com>
2023-03-10 16:01:46 +0200

    add a href

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const div = document.createElement("div");
+const header = document.createElement("h1");
+header.textContent = " Learn js";
+div.appendChild(header);
+const a = document.createElement("a");
+a.href = "https://www.ukr.net";
+a.textContent = "Ukr.net";
+document.body.appendChild(a)
 
+document.body.appendChild(div);
 
 console.log(div);
+console.log(header);
+console.log(a);
